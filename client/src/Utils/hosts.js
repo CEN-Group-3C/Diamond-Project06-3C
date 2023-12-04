71bd0323304a6ce94a1346dce377b0f6eacad9d7
Isaac Hunter <ikehunter5@gmail.com>
2023-12-04 14:19:35 -0500

    change dockerfile back to alpine

--- client/src/Utils/hosts.js
+++ client/src/Utils/hosts.js
@@ -2,5 +2,5 @@
 const { hostname } = window.location
 
 // export the server host
-// export const server = hostname.includes('localhost') ? 'http://localhost:1337/api' : '/api'
-export const server = 'http://10.136.54.45:1337/api'
+export const server = hostname.includes('localhost') ? 'http://localhost:1337/api' : '/api'
+// export const server = 'http://10.136.54.45:1337/api'
